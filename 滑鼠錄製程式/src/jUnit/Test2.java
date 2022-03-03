54fffc0f906f4c96c466d8c7259fc9ae4d964d40
SaviorDT <45823994+SaviorDT@users.noreply.github.com>
2022-03-03 22:58:29 +0800

    Add files via upload

---
+++ 滑鼠錄製程式/src/jUnit/Test2.java
@@ -0,0 +1,21 @@
+package jUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class Test2 {
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		assertEquals(1, 1);
+	}
+	
+	@org.junit.jupiter.api.Test
+	@DisplayName("³o­Ó·|¿ù")
+	void test2() {
+		assertEquals(1, 2);
+	}
+
+}
